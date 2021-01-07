ec11ad7ca476cda78762e67277e18a420fae65ff
Mohammed Bettaoui <bettapro100@gmail.com>
2021-01-07 12:54:48 +0100

    structure Project

---
+++ src/application/models/Admin.java
@@ -0,0 +1,55 @@
+package application.models;
+
+public class Admin {
+	
+	public int id;
+	public String username;
+	public String password;
+	
+	
+	public Admin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Admin(int id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
